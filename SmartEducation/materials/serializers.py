5c23c88ddc5a7a45acb0322075ac9b84654a6ad4
Doczadrot <kisilevrodion@gmail.com>
2025-08-13 22:31:27 +0300

    Добавил новую модель в приложении пользователя:  Оплаты.

--- SmartEducation/materials/serializers.py
+++ SmartEducation/materials/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
+from rest_framework.serializers import SerializerMetaclass
+
 from .models import Lesson, Course
 
 # Сериализатор для модели Course
 class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+class CourseDetailSerializer(serializers.ModelSerializer):
+
+    count_lesson = SerializerMethodField() # добавляем поле количество уроков
+
+    def get_count_lesson(self, instance): #описываем как будет работать count_lesson
+        return instance.lesson_set.count()
+
     class Meta:
         model = Course
         fields = '__all__'
--- SmartEducation/users/models.py
+++ SmartEducation/users/models.py
@@ -2,7 +2,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
-
+from django.db.models import CASCADE
+from materials.models import Lesson, Course
 
 class Users(AbstractUser):
     phone_number = PhoneNumberField(
@@ -17,3 +18,21 @@ class Users(AbstractUser):
     help_text='Группы, к которым принадлежит этот пользователь. Пользователь получит все права, предоставленные каждой из его групп',
         related_name="customuser_set",  # Важно для избежания конфликтов related_name
         related_query_name="customuser")
+
+
+class Pays(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now=True)
+    paid_course = models.ForeignKey(Course, on_delete=CASCADE, null=True, blank=True, verbose_name='Оплаченный курс')
+    paid_lesson = models.ForeignKey(Lesson, on_delete=CASCADE, null=True, blank=True, verbose_name='Оплаченный урок')
+    summ_pays = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Сумма оплаты')
+    variant_pays = models.CharField(max_length=20, choices=(('chash', 'Наличные'),
+                                                            ('transfer', 'Перевод')))
+
+    def __str__(self):
+        return f"Платеж от {self.user.email} на сумму {self.summ_pays} за {self.paid_course if self.paid_course else self.paid_lesson}"
+
+    class Meta:
+        verbose_name = 'Платеж'
+        verbose_name_plural = 'Платежи'
+        ordering = ['-date']
